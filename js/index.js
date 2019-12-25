702569aafeeb1504b4de96e8d4e58e09e10ca0ea
Myles Young <myles.young@mailchimp.com>
2019-12-25 15:01:31 -0500

    day 2 looking good 💪🏽

--- js/index.js
+++ js/index.js
@@ -67,5 +67,50 @@ let repeatedDataSums = data.reduce((array, key) => {
     array[key]++;
     return array;
 }, {});
+// console.log(repeatedDataSums);
 
-console.log(repeatedDataSums);
+// ## Array Cardio Day 2
+const otherPeople = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'God is great!', id: 1 }
+];
+
+// Some and Every Checks
+
+// is at least one person 19 or older?
+const isNineteenOrOlder = (person) => person.year >= 2000;
+// console.log(otherPeople.some(isNineteenOrOlder));
+// is everyone 19 or older?
+// console.log(otherPeople.every(isNineteenOrOlder));
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+let specialUser = comments.find( number => {
+    return number.id == 1
+});
+// console.log(specialUser);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+let removedUser = comments.findIndex( number => {
+    return number.id == 823423
+ });
+
+const newComments = [
+    ...comments.slice(0, removedUser), //spread so it won't be an array of arrays that gets returned
+    ...comments.slice(removedUser + 1), // move to index after removedItem and spread the rest of the array in there
+];
+
+console.table(newComments);
